025598027b590c41c03abdc457bcfa8cedc6999b
tech4property <100059746+tech4property@users.noreply.github.com>
2022-07-10 20:09:35 +0530

    added tailwind css and partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
 
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: vercel()
+  adapter: vercel(),
+  integrations: [react(), tailwind(), partytown()]
 });
